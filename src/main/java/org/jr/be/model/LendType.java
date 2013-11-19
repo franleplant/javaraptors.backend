a0ec1866f248fb3503c6f5051ad2924bcc145f96
franleplant <franleplant@gmail.com>
2013-11-19 20:18:02 -0300

    Add lend type model class

---
+++ src/main/java/org/jr/be/model/LendType.java
@@ -0,0 +1,75 @@
+package org.jr.be.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+// This class will be instantiated in to two objects 
+// containing foreign | local and it is related to the LendTypes
+
+@Entity
+public class LendType {
+	
+	// Synthetic id
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	@NotEmpty
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}	
+
+	@Override
+	public String toString() {
+		return type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LendType other = (LendType) obj;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+
+
+}
