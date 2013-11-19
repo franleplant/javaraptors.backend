dbbdad537f1d80cbd8d7597e3bbb20bff3a02a8e
franleplant <franleplant@gmail.com>
2013-11-19 20:43:18 -0300

    Add hollyday to the persistence layer

---
+++ src/main/java/org/jr/be/model/Hollyday.java
@@ -0,0 +1,77 @@
+package org.jr.be.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+public class Hollyday {
+	
+	// Synthetic id
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	@NotEmpty
+	private Date date;
+	
+	private String comments;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hollyday other = (Hollyday) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		return true;
+	}
+
+
+}
