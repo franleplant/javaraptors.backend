e93ad0f70ec381ac9a6cdd93dd3721498d5575ee
franleplant <franleplant@gmail.com>
2013-11-19 20:21:56 -0300

    add EntityType

---
+++ src/main/java/org/jr/be/model/EntityType.java
@@ -0,0 +1,84 @@
+package org.jr.be.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+/* This class will be used as a container of entity types
+ *  that are needed in order for the front end to process the
+ *  different types correctly
+ * */
+
+@Entity
+public class EntityType {
+
+	// Synthetic id
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	
+	
+	@Column(unique = true)
+	@NotEmpty
+	private String type;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	@Override
+	public String toString() {
+		return type;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EntityType other = (EntityType) obj;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+	
+	
+}
