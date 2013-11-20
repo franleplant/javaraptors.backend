fbf29a60b122008e1489c699e9e126e68b2c2c1e
franleplant <franleplant@gmail.com>
2013-11-20 15:52:21 -0300

    Update Audit Entity

--- src/main/java/org/jr/be/model/Audit.java
+++ src/main/java/org/jr/be/model/Audit.java
@@ -1,40 +1,36 @@
 package org.jr.be.model;
 
+import static javax.persistence.TemporalType.DATE;
+
+import java.util.Date;
+
 import javax.persistence.Embeddable;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Embeddable
 public class Audit {
 	
-	private int removeMe;
-
-	public int getRemoveMe() {
-		return removeMe;
-	}
-
-	public void setRemoveMe(int removeMe) {
-		this.removeMe = removeMe;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + removeMe;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Audit other = (Audit) obj;
-		if (removeMe != other.removeMe)
-			return false;
-		return true;
-	}
+	@NotEmpty
+	@OneToOne
+	private User createUser;
+	
+	@NotEmpty
+	@Temporal(DATE)
+	private Date createDate;	
+	
+	@OneToOne
+	private User editUser;
+	
+	@Temporal(DATE)
+	private Date editDate;	
+	
+	@OneToOne
+	private User deleteUser;
+	
+	@Temporal(DATE)
+	private Date deleteDate;
 
 }
