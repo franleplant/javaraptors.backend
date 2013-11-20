39d60ca4ffdb894c9c1eaa14e3a5d555c45c4445
franleplant <franleplant@gmail.com>
2013-11-20 15:41:17 -0300

    Add Affiliate

---
+++ src/main/java/org/jr/be/model/Affiliate.java
@@ -0,0 +1,156 @@
+package org.jr.be.model;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.EAGER;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+@Entity
+public class Affiliate {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	
+	private double reputation;
+	
+	@NotEmpty
+	@OneToOne(cascade = ALL)
+	private Person person;
+	
+	@Embedded
+    private Audit audit = new Audit();
+	
+	@NotEmpty
+	private EntityType type;
+	
+	@OneToMany(cascade = ALL, fetch = EAGER)
+    private Set<Suspension> suspensions = new HashSet<Suspension>();
+	
+	@NotEmpty
+	private Boolean deleted;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getReputation() {
+		return reputation;
+	}
+
+	public void setReputation(double reputation) {
+		this.reputation = reputation;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Audit getAudit() {
+		return audit;
+	}
+
+	public void setAudit(Audit audit) {
+		this.audit = audit;
+	}
+
+	public EntityType getType() {
+		return type;
+	}
+
+	public void setType(EntityType type) {
+		this.type = type;
+	}
+
+	public Set<Suspension> getSuspensions() {
+		return suspensions;
+	}
+
+	public void setSuspensions(Set<Suspension> suspensions) {
+		this.suspensions = suspensions;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((audit == null) ? 0 : audit.hashCode());
+		result = prime * result + ((deleted == null) ? 0 : deleted.hashCode());
+		result = prime * result + ((person == null) ? 0 : person.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(reputation);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Affiliate other = (Affiliate) obj;
+		if (audit == null) {
+			if (other.audit != null)
+				return false;
+		} else if (!audit.equals(other.audit))
+			return false;
+		if (deleted == null) {
+			if (other.deleted != null)
+				return false;
+		} else if (!deleted.equals(other.deleted))
+			return false;
+		if (person == null) {
+			if (other.person != null)
+				return false;
+		} else if (!person.equals(other.person))
+			return false;
+		if (Double.doubleToLongBits(reputation) != Double
+				.doubleToLongBits(other.reputation))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Affiliate [reputation=" + reputation + ", person=" + person
+				+ ", deleted=" + deleted + "]";
+	}
+	
+}
