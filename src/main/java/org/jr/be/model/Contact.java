cbbfbd37aa020fd4fd09f051237ff3980944e307
franleplant <franleplant@gmail.com>
2013-11-19 23:55:38 -0300

    Add Contact

---
+++ src/main/java/org/jr/be/model/Contact.java
@@ -0,0 +1,122 @@
+package org.jr.be.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+@Entity
+public class Contact {
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	
+	@NotEmpty
+	private String email;
+	
+	private Long tel;
+	
+	private Long cel;
+	
+	private String web;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getTel() {
+		return tel;
+	}
+
+	public void setTel(Long tel) {
+		this.tel = tel;
+	}
+
+	public Long getCel() {
+		return cel;
+	}
+
+	public void setCel(Long cel) {
+		this.cel = cel;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cel == null) ? 0 : cel.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((tel == null) ? 0 : tel.hashCode());
+		result = prime * result + ((web == null) ? 0 : web.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (cel == null) {
+			if (other.cel != null)
+				return false;
+		} else if (!cel.equals(other.cel))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (tel == null) {
+			if (other.tel != null)
+				return false;
+		} else if (!tel.equals(other.tel))
+			return false;
+		if (web == null) {
+			if (other.web != null)
+				return false;
+		} else if (!web.equals(other.web))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [email=" + email + ", tel=" + tel + ", cel=" + cel
+				+ ", web=" + web + "]";
+	}
+}
