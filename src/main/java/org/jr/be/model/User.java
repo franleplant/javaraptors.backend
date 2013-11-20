9104d4a0f064d43638cd9197e814dd6db380f2f5
franleplant <franleplant@gmail.com>
2013-11-20 15:47:26 -0300

    Add User: TODO: what to do with the Rol property, should be an Enumeration, another Entity

---
+++ src/main/java/org/jr/be/model/User.java
@@ -0,0 +1,180 @@
+package org.jr.be.model;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	
+	
+	@NotEmpty
+	private String password;
+	
+	@NotEmpty
+	private String rol;
+		
+	@NotEmpty
+	@OneToOne(cascade = ALL)
+	private Person person;
+	
+	@Embedded
+    private Audit audit = new Audit();
+	
+	@NotEmpty
+	private EntityType type;
+	
+	
+	@NotEmpty
+	private Boolean deleted;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getRol() {
+		return rol;
+	}
+
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+
+	public Person getPerson() {
+		return person;
+	}
+
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+
+	public Audit getAudit() {
+		return audit;
+	}
+
+
+	public void setAudit(Audit audit) {
+		this.audit = audit;
+	}
+
+
+	public EntityType getType() {
+		return type;
+	}
+
+
+	public void setType(EntityType type) {
+		this.type = type;
+	}
+
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((audit == null) ? 0 : audit.hashCode());
+		result = prime * result + ((deleted == null) ? 0 : deleted.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((person == null) ? 0 : person.hashCode());
+		result = prime * result + ((rol == null) ? 0 : rol.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (audit == null) {
+			if (other.audit != null)
+				return false;
+		} else if (!audit.equals(other.audit))
+			return false;
+		if (deleted == null) {
+			if (other.deleted != null)
+				return false;
+		} else if (!deleted.equals(other.deleted))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (person == null) {
+			if (other.person != null)
+				return false;
+		} else if (!person.equals(other.person))
+			return false;
+		if (rol == null) {
+			if (other.rol != null)
+				return false;
+		} else if (!rol.equals(other.rol))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [password=" + password + ", rol=" + rol + ", person="
+				+ person + ", type=" + type + ", deleted=" + deleted + "]";
+	}
+
+
+	
+}
