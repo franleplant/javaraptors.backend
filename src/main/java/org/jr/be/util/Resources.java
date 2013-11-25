a92958c7fac7c26f1691a44a01fd4a10ca0baae8
franleplant <franleplant@gmail.com>
2013-11-25 11:04:21 -0300

    add Entity manager producer

---
+++ src/main/java/org/jr/be/util/Resources.java
@@ -0,0 +1,40 @@
+package org.jr.be.util;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+/**
+ * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
+ * 
+ * <p>
+ * Example injection on a managed bean field:
+ * </p>
+ * 
+ * <pre>
+ * &#064;Inject
+ * private EntityManager em;
+ * </pre>
+ */
+public class Resources {
+
+   @Produces
+   @PersistenceContext
+   private EntityManager em;
+   
+  
+   /**
+    * Provider injectable loggers based around Java Util Logging.
+    * @param injectionPoint
+    * @return
+    */
+   @Produces
+   public Logger produceLog(InjectionPoint injectionPoint) {
+      return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+   }
+   
+}
