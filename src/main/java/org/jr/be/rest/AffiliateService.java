bf9fc89600f0f7245b2df7845cc342ee421af127
franleplant <franleplant@gmail.com>
2013-11-26 18:06:13 -0300

    affiliate creation API is now fully functional (exclamation mark), wiiiiiii (double exclamation mark). Had to add javax.transaction JTA depenency

--- src/main/java/org/jr/be/rest/AffiliateService.java
+++ src/main/java/org/jr/be/rest/AffiliateService.java
@@ -1,15 +1,22 @@
 package org.jr.be.rest;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import javax.transaction.UserTransaction;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.jr.be.model.Affiliate;
+import org.jr.be.model.Audit;
 
 
 @Path("/affiliate")
@@ -19,6 +26,9 @@ public class AffiliateService {
     
     @PersistenceUnit(unitName = "primary")
     private EntityManagerFactory entityManagerFactory;
+    
+    @Resource
+    private UserTransaction u;
         
         @GET
         @Path("/{id:[0-9][0-9]*}")
@@ -37,5 +47,36 @@ public class AffiliateService {
 	        
 	        return affiliate;        
         }
+        
+        @POST
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public String create(Affiliate affiliate) throws Exception {
+        	
+        	
+        	affiliate.setDeleted(false);
+        	Audit audit = new Audit();
+        	
+        	//http://docs.oracle.com/javase/6/docs/api/java/util/Date.html
+        	//This references to right now
+        	Date date = new Date();
+        	
+        	audit.setCreateDate(date);
+        	
+        	// When login is done do this:
+        	//audit.setCreateUser(createUser);
+        	
+        	affiliate.getPerson().setAudit(audit);
+        	
+        	u.begin();
+        	EntityManager entityManager = entityManagerFactory.createEntityManager(); 
+        	
+        	entityManager.merge(affiliate);
+        	entityManager.flush();
+        	u.commit();
+            entityManager.close();
+            
+        	return "{status: ok}";
+        };
         
 }
