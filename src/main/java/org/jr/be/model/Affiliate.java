dea5be69181a8ee96bb3707df2aff34be2c65f5b
franleplant <franleplant@gmail.com>
2013-11-26 10:30:50 -0300

    Person is now an embeddable entity

--- src/main/java/org/jr/be/model/Affiliate.java
+++ src/main/java/org/jr/be/model/Affiliate.java
@@ -6,13 +6,13 @@ import static javax.persistence.GenerationType.IDENTITY;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 
@@ -25,9 +25,8 @@ public class Affiliate {
 	
 	private double reputation;
 	
-	@NotNull
-	@OneToOne(cascade= ALL)
-	private Person person;
+	@Embedded
+	private Person person = new Person();
 	
 	@NotNull
 	@ManyToOne(fetch = FetchType.EAGER)
