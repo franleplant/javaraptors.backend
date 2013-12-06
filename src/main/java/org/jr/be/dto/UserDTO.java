250a5fcc7b33b6107304392f932d48179805eef5
franleplant <franleplant@gmail.com>
2013-12-06 03:16:38 -0300

    user validation with database plus jason respond with status

---
+++ src/main/java/org/jr/be/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package org.jr.be.dto;
+
+import java.util.HashSet;
+
+
+public class UserDTO {
+
+	private String user;
+	
+	private String pass;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+}
