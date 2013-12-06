d38bc70f8b62b1c6672c1902f72522ce67572fb5
franleplant <franleplant@gmail.com>
2013-12-05 22:40:46 -0300

    copy lend get is working great now

--- src/main/java/org/jr/be/rest/CopyLendService.java
+++ src/main/java/org/jr/be/rest/CopyLendService.java
@@ -1,10 +1,11 @@
 package org.jr.be.rest;
 
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceUnit;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,6 +17,7 @@ import javax.ws.rs.core.Response;
 
 import org.jr.be.dto.CopyLendGetDTO;
 import org.jr.be.model.Copy;
+import org.jr.be.model.Hollyday;
 import org.jr.be.model.LendType;
 
 
@@ -28,7 +30,7 @@ public class CopyLendService {
 //    @Resource
 //    private UserTransaction u;
     
-    private Date getMaxReturnDate(  String lendType ) {
+    private Date getMaxReturnDate(  String lendType, EntityManager em ) {
     	Date max;
     	
 
@@ -36,9 +38,59 @@ public class CopyLendService {
         if (  lendType == "local" ) {
         	//If it has only a local lend then the affiliate should return it the same day the he/she took it
         	max = new Date();
+        	
         } else {
+        	
         	//Holyday logic
         	max = new Date();
+        	
+        	Calendar cal = Calendar.getInstance();
+        	cal.setTime( new Date() );
+     	
+        	Hollyday hollyday;
+        	int working_days = 0;
+        	int total_days = 0;
+        	
+            
+            
+            while (working_days < 10) {
+                cal.add(Calendar.DAY_OF_MONTH, 1);
+                if (cal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && cal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                	
+                	//es holyday?
+                	
+                	try {
+	                	hollyday = em.createQuery(
+	                    	    "FROM Hollyday as h WHERE h.date = ?1", Hollyday.class)
+	                    	    .setParameter(1, cal.getTime() )
+	                    	    .getSingleResult();
+                	
+	                	//Test if a hollyday in the given date has been found
+	                	//hollyday.getId();
+	                	
+                	} catch(NoResultException ex) {
+                		//If not
+                		working_days++;
+                	}
+                	
+                }
+                
+                total_days++;
+            }
+            
+            System.out.println("Working days");
+            System.out.println(working_days);
+            System.out.println("Total Days");
+            System.out.println(total_days);
+            
+            
+        	Calendar cal_end = Calendar.getInstance();
+        	cal_end.setTime( new Date() );
+        	cal_end.add( Calendar.DATE, total_days);
+        	
+        	max = cal_end.getTime();
+        	
+
         }
         
     	return max;
@@ -66,13 +118,22 @@ public class CopyLendService {
         
         String parent_lend_type = "local";
         for (LendType lendType : copy.getLendTypes()) {
-        	if ( lendType.getName() == "foreign" ) {
+            System.out.println("Iteration");
+            System.out.println(lendType.getName());
+            //getName is not matchig, dont know why
+        	if ( lendType.getId() == 2 ) {
         		parent_lend_type = "foreign";
+                System.out.println("Inside the if");
+                System.out.println(parent_lend_type);
+        		break;
         	}
         };
         
+        System.out.println("Parent Type Lend");
+        System.out.println(parent_lend_type);
+        
 
-        dto.getDate().setMax(  getMaxReturnDate(parent_lend_type)  );
+        dto.getDate().setMax(  getMaxReturnDate(parent_lend_type, entityManager)  );
         
        	
         entityManager.close();
