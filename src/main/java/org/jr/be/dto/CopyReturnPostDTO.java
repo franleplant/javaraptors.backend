7ca95bd9422e62d5f7e9e8fac274f105d818fd4d
franleplant <franleplant@gmail.com>
2013-12-07 00:10:55 -0300

    full lend return workflow is now working

---
+++ src/main/java/org/jr/be/dto/CopyReturnPostDTO.java
@@ -0,0 +1,24 @@
+package org.jr.be.dto;
+
+public class CopyReturnPostDTO {
+
+	//copy id
+	private long id;
+	private String lend_comments;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getLend_comments() {
+		return lend_comments;
+	}
+	public void setLend_comments(String lend_comments) {
+		this.lend_comments = lend_comments;
+	}
+	
+	
+	
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -69,6 +69,9 @@ insert into LEND (id, actualreturndate, expectedreturndate, lenddate, affiliate_
 --Insert a current lend
 insert into lend (id, lenddate,  expectedreturndate, affiliate_id, copy_id , lendtype_id, lendinguser_id  ) values (2, '2013-11-29', '2013-12-30', 1, 2, 2, 1);
 
+--Insert a delayed lend
+insert into LEND (id, expectedreturndate, lenddate, affiliate_id, copy_id, lendtype_id, lendinguser_id) values (3, '2013-10-15', '2013-10-10', 1, 1, 2, 1);
+
 
 insert into hollyday (id, date, comments ) values (1, '2013-12-10', 'Dia de los hombres con acento judio que no son judios');
 
