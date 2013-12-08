9f89dfd632478d99ba1be7e8621184b0f6342df1
franleplant <franleplant@gmail.com>
2013-12-08 00:39:49 -0300

    integrating with frontend

--- src/main/java/org/jr/be/rest/LogoutServlet.java
+++ src/main/java/org/jr/be/rest/LogoutServlet.java
@@ -1,13 +1,7 @@
 package org.jr.be.rest;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceUnit;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -15,13 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
-
-import org.jr.be.dto.AffiliateCopyDTO;
-import org.jr.be.dto.UserDTO;
-import org.jr.be.model.User;
-import org.jr.be.util.JsonResponseMsg;
 
 
 @WebServlet("/LogoutServlet")
@@ -31,36 +18,41 @@ public class LogoutServlet extends HttpServlet {
 	
    	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
-    	
-		HttpSession session = request.getSession();
-    	
-
-    	System.out.println("----------- Esta clase anda, LogoutServlet.java  -----------");
-    	
-    	System.out.println("----------- " + session.getId() + "  -----------");
-
-    }
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//    	
+//    	
+//		HttpSession session = request.getSession();
+//    	
+//
+//    	System.out.println("----------- Esta clase anda, LogoutServlet.java  -----------");
+//    	
+//    	System.out.println("----------- " + session.getId() + "  -----------");
+//
+//    }
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         Cookie[] cookies = request.getCookies();
         if(cookies != null){
         for(Cookie cookie : cookies){
             if(cookie.getName().equals("JSESSIONID")){
                 System.out.println("JSESSIONID="+cookie.getValue());
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
                 break;
             }
         }
         }
         //invalidate the session if exists
-        HttpSession session = request.getSession(false);
+        HttpSession session = request.getSession();
         System.out.println("----------- User="+session.getAttribute("user"));
         if(session != null){
             session.invalidate();
         }
-        response.sendRedirect("login.html");
+        
+        response.sendRedirect("frontend/app/login.html");
     }
 
 }
