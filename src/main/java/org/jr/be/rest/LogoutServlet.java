18801280fe6d835b07876e4e1dadeebff554c029
franleplant <franleplant@gmail.com>
2013-12-08 00:52:03 -0300

    fixes + example of how to protect rest resources

--- src/main/java/org/jr/be/rest/LogoutServlet.java
+++ src/main/java/org/jr/be/rest/LogoutServlet.java
@@ -46,7 +46,7 @@ public class LogoutServlet extends HttpServlet {
         }
         }
         //invalidate the session if exists
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(false);
         System.out.println("----------- User="+session.getAttribute("user"));
         if(session != null){
             session.invalidate();
